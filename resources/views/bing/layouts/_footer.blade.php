6bfd3873b5d391907d3dbc4c4923b14c72fb89b6
瑞创云 <112141097+16602038811@users.noreply.github.com>
2024-01-07 19:21:35 +0800

    1

---
+++ resources/views/bing/layouts/_footer.blade.php
@@ -0,0 +1,14 @@
+<!-- footer start-->
+<footer class="footer p-3">
+    <div class="row">
+        <div class="col-12 text-center text-white">
+            <div class="author">
+                Powered by：<a href="http://qx7c.cn/">瑞创云</a>
+            </div>
+            <div class="custom">
+                {!! dujiaoka_config_get('footer') !!}
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer end-->
